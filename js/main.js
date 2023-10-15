3eae0546547a49b2fa43f7464c640ec87fcd1c20
ozawa <trizweb1060@gmail.com>
2023-10-15 11:17:05 +0900

    favicon add

--- js/main.js
+++ js/main.js
@@ -45,3 +45,16 @@ jQuery(window).bind("scroll", function () {
   weight2 = 0.12;
   jQuery(".parallax1").css("top", 300 - scrolled * weight1 + "px");
 });
+//slick
+$(function () {
+  $("#js-slider-2").slick({
+    arrows: true, // 前・次のボタンを表示する
+    dots: true, // ドットナビゲーションを表示する
+    appendDots: $(".dots-2"), // ドットナビゲーションの生成位置を変更
+    speed: 1000, // スライドさせるスピード（ミリ秒）
+    slidesToShow: 1, // 表示させるスライド数
+    centerMode: true, // slidesToShowが奇数のとき、現在のスライドを中央に表示する
+    variableWidth: true, // スライド幅の自動計算を無効化
+    autoplay: true,
+  });
+});
